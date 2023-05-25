ffd52c8b8ec429a30383d6ba9a77ce345e64c330
Phan Ngọc Lunah Hà <ngochaphan.work@gmail.com>
2023-05-25 19:04:32 +0700

    Commit using git to reponsive

--- javascript.js
+++ javascript.js
@@ -1,26 +1,24 @@
-setInterval(greeting, 2000);
-function greeting() {
-    let greet = new Array("Hello", "Xin Chào", "Bonjour", "안녕하세요", "Hola");
-    let iam = new Array("I am", "Tôi là", "Je suis", "나는", "Yo soy");
-    let i = 0;
-    let j = 0;
-    let elemgreet = $(".text--greeting");
-    let elemiam = $("#iam");
-    elemgreet.fadeOut(function () {
-        elemgreet.html(greet[i] + "!");
-        i++;
-        if (i >= greet.length) { i = 0; }
-        elemgreet.fadeIn();
-    });
-    elemiam.fadeOut(function () {
-        elemiam.html(" " + iam[j]);
-        j++;
-        if (j >= iam.length) { j = 0; }
-        elemiam.fadeIn();
-    });
+// Greeting Interval with Delay
 
-}
+var arrSalute = new Array("Hello", "Xin Chào", "Bonjour", "안녕하세요", "Hola");
+var elmSalute = document.querySelector(".text--greeting");
+var arrIAm = new Array("I am", "Tôi là", "Je suis", "나는", "Yo soy");
+let elemIam = document.querySelector("#iam");
+intervalGreet(arrSalute,elmSalute);
+intervalGreet(arrIAm,elemIam);
+function intervalGreet(arr,elmArr){
+    displayGreetWithDelay();
+    setInterval(displayGreetWithDelay,1000*arr.length);
+    function displayGreetWithDelay(){
+        arr.forEach((object, index) => {
+            setTimeout(function () {
+                elmArr.innerHTML = object;
+            }, 1000 * index);
+        });
+    };
+}    
 
+//Section Display and Navbar Effect when Scroll
 scrollAnimation();
 function scrollAnimation() {
     let section = document.querySelectorAll("section");
@@ -50,27 +48,52 @@ function scrollAnimation() {
     };
 }
 
-function changeTheme() {
-    changeColorHeaderFromWhiteToGray();
-    changeColorTextFromWhitetoBlack();
-}
+//Navbar Button Onclick  
+const navDropDownBtn = document.querySelector('.navbar-btn-toggle');
+const navDropDown = document.querySelector('.navbar');
+const navDropDownIcon = document.querySelectorAll('.navbar-btn-toggle i');
 
-function changeColorHeaderFromWhiteToGray() {
+navDropDownBtn.onclick = function(){
+    navDropDown.classList.toggle("display-flex");
+    navDropDownIcon.forEach((i) => {
+        if (i.getAttribute("class").includes("display-block")) {
+            i.classList.remove("display-block");
+            i.classList.add("display-none");
+        }
+        else if (i.getAttribute("class").includes("display-none")) {
+            i.classList.add("display-block");
+            i.classList.remove("display-none");
+        }
+    });
+
+};
+
+
+//Theme ( color of Background/Text ) when click Icon 
+
+const themeBtn = document.querySelector('#themeBtn');
+themeBtn.onclick = function(){
+    changeHeaderColor();
+    changeTextColor();
+};
+
+function changeHeaderColor() {
     let bg_header = document.querySelector("header");
-    console.log(bg_header);
     bg_header.classList.toggle("bg-color-gray-700");
 }
-function changeColorTextFromWhitetoBlack() {
+
+function changeTextColor() {
     let menu = document.querySelectorAll(".navbar ul li a");
-    console.log(menu);
     menu.forEach((i) => {
-        if (i.getAttribute("class").includes("text-color-black")){
+        if (i.getAttribute("class").includes("text-color-black")) {
             i.classList.remove("text-color-black");
             i.classList.add("text-color-white");
-        } 
-        else if(i.getAttribute("class").includes("text-color-white")) {
+        }
+        else if (i.getAttribute("class").includes("text-color-white")) {
             i.classList.add("text-color-black");
             i.classList.remove("text-color-white");
         }
     });
 }
+
+
